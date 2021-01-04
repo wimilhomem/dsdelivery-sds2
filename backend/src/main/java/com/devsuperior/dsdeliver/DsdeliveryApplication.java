7196b1bd3d4db8f0108f3296d78efd211ae2b153
wisley <wisleycristiano@gmail.com>
2021-01-04 18:17:35 -0300

    initial setup

--- backend/src/main/java/com/devsuperior/dsdeliver/DsdeliveryApplication.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/DsdeliveryApplication.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dsdelivery;
+package com.devsuperior.dsdeliver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
